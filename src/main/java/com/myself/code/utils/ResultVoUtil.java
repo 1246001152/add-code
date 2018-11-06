b630cfe711c4a6f028f3a9be6077e65d7f4e437d
hacker <3183764662@qq.com>
2018-11-06 20:31:30 +0800

    添加功能代码

---
+++ src/main/java/com/myself/code/utils/ResultVoUtil.java
@@ -0,0 +1,32 @@
+package com.myself.code.utils;
+
+import com.myself.code.vo.ResultVO;
+
+/**
+ * @Author:UncleCatMySelf
+ * @Email：zhupeijie_java@126.com
+ * @QQ:1341933031
+ * @Date:Created in 19:58 2018\11\6 0006
+ */
+public class ResultVoUtil {
+
+    public static ResultVO success(Object object){
+        ResultVO resultVO = new ResultVO();
+        resultVO.setData(object);
+        resultVO.setCode(200);
+        resultVO.setMsg("成功");
+        return resultVO;
+    }
+
+    public static ResultVO success(){
+        return success(null);
+    }
+
+    public static ResultVO error(Integer code, String msg){
+        ResultVO resultVO = new ResultVO();
+        resultVO.setCode(code);
+        resultVO.setMsg(msg);
+        return resultVO;
+    }
+
+}
